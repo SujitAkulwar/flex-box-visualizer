9b00e6be775a69e146bde8a0247c7c1af1600f29
sujit akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2023-01-26 22:54:32 +0530

    new day new button close open complete partially

--- src/script.js
+++ src/script.js
@@ -15,72 +15,67 @@ var aligncontent = ["0","content-center","content-start","content-end","content-
 
 var flexbox = [0, flexDirection, JustifyContent, flexwrap, alignitems, aligncontent];
 
-var bodysubcontaint = document.getElementById("bodyhide");
-var bodybutton = document.getElementById('l0');
-bodysubcontaint.style.display = "none";
 
-var counter1 = 0;
-   
+var bodysubcontaint = document.getElementById("bodyhide");
+var block1 = document.getElementById("block1hide");
+var block2 = document.getElementById("block2hide");
+var block3 = document.getElementById("block3hide");
+var block4 = document.getElementById("block4hide");
+
+
+var hidearray = [bodysubcontaint, block1, block2, block3, block4];
+var counter = [0, 0, 0, 0, 0];
+
+hide(0);hide(1);hide(2);hide(3);hide(4); 
+//body hide function 
+function hide(e){
+    counter[e]++;
+    if(counter[e]% 2 === 0){
+        hidearray[e].style.display = "block";
+    }else {
+        hidearray[e].style.display = "none";
+    }
+}
+
+
 submit.onclick =  (e) =>{ // main function  
-    // console.log(poo.value);
     if(poo.value=="0"){
-        console.log("display not used");
         bodys.classList.remove("flex");
     }else{
-        console.log("display flex is used");
         bodys.classList.add("flex");
     }
     if(poi.value=="0"){
-        console.log(" Flex Direction not used");
         removeclass(1);
     }else{
-        console.log('Flex Direction : '+flexDirection[poi.value]+' is used');
         removeclass(1);
         bodys.classList.add(flexDirection[poi.value]);
     }
     if(pot.value==="0"){
-        console.log("Justify-Content not used");
         removeclass(2);
     }else{
-        console.log('Justify-Content : '+JustifyContent[pot.value]+' is used')
         removeclass(2);
         bodys.classList.add(JustifyContent[pot.value]);
     }
     if(poe.value==="0"){
-        console.log("flexwrap not used");
         removeclass(3);
     }else{
-        console.log('flexwrap : '+flexwrap[poe.value]+' is used')
         removeclass(3);
         bodys.classList.add(flexwrap[poe.value]);
     }
     if(por.value==="0"){
-        console.log("align item not used");
         removeclass(4);
     }else{
-        console.log('align item : '+alignitems[por.value]+' is used')
         removeclass(4);
         bodys.classList.add(alignitems[por.value]);
     }
     if(pof.value==="0"){
-        console.log("align content not used");
         removeclass(5);
     }else{
-        console.log('align content : '+aligncontent[pof.value]+' is used')
         removeclass(5);
         bodys.classList.add(aligncontent[pof.value]);
     }
 }
 
-//body hide function 
-bodybutton.addEventListener("click", function(){
-    counter1 = counter1 + 1;
-    if(counter1% 2 === 0){
-        bodysubcontaint.style.display = "none";
-    }else {
-        bodysubcontaint.style.display = "block";
-    }
-});
 
 //combine function to remove class
 function removeclass(e) {
