2bcfdf696c7aa5d3b78e56492faea5d6389093f2
sujit akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2023-01-30 14:33:52 +0530

    complete ig
    still few changes possible

--- src/script.js
+++ src/script.js
@@ -13,29 +13,30 @@ var pof = document.querySelector("#p05");
 var o1 = document.getElementById("o1");
 var fg1 = document.getElementById("fg1");
 var fs1 = document.getElementById("fs1");
-var flexbasis1 = document.getElementById("flexbasis1");
 var as1 = document.getElementById("as1");
+
 var o2 = document.getElementById("o2");
 var fg2 = document.getElementById("fg2");
 var fs2 = document.getElementById("fs2");
-var flexbasis2 = document.getElementById("flexbasis2");
 var as2 = document.getElementById("as2");
+
 var o3 = document.getElementById("o3");
 var fg3 = document.getElementById("fg3");
 var fs3 = document.getElementById("fs3");
-var flexbasis3 = document.getElementById("flexbasis3");
 var as3 = document.getElementById("as3");
+
 var o4 = document.getElementById("o4");
 var fg4 = document.getElementById("fg4");
 var fs4 = document.getElementById("fs4");
-var flexbasis4 = document.getElementById("flexbasis4");
+
 var as4 = document.getElementById("as4");
+
 //
 var boxarray = [[o1, fg1, fs1, as1], [o2, fg2, fs2, as2], [o3, fg3, fs3, as3], [o4, fg4, fs4, as4]];
 var boxclassarray = [
     ["0", "order-1", "order-2", "order-3", "order-4", "order-first", "order-last"],
-    ["0", "1", "2"],
-    ["0", "1", "2"],
+    ["0", "grow-0", "grow"],
+    ["0", "shrink-0", "shrink"],
     ["0", "self-auto", "self-start", "self-end", "self-center", "self-stretch", "self-baseline"]];                                           
 
 var b1 = document.getElementById('b1');
@@ -64,17 +65,44 @@ var block4 = document.getElementById("block4hide");
 var hidearray = [bodysubcontaint, block1, block2, block3, block4];
 var counter = [0, 0, 0, 0, 0];
 
-for (let i = 0; i < 5; i++){
+for (var i = 0; i < 5; i++){
     hide(i);
 }
+
 
 //body hide function 
 function hide(e){
     counter[e]++;
-    if(counter[e]% 2 === 0){
+    var t = e;
+    if(counter[e]% 2 == 0){
         hidearray[e].style.display = "block";
+        rotatefun(t, 0);
     }else {
         hidearray[e].style.display = "none";
+        rotatefun(t, 1);
+    }
+}
+
+function rotatefun(e,i) {
+    if (e == 0) {
+        if (i == 0) { document.getElementById('i0').setAttribute("style", "transform:rotate(90deg);"); }
+        else { document.getElementById('i0').setAttribute("style", "transform:rotate(0deg);"); }
+    }
+    else if (e == 1) {
+        if (i == 0) { document.getElementById('i1').setAttribute("style", "transform:rotate(90deg);"); }
+        else { document.getElementById('i1').setAttribute("style", "transform:rotate(0deg);"); }
+    }
+    else if (e == 2) {
+        if (i == 0) { document.getElementById('i2').setAttribute("style", "transform:rotate(90deg);"); }
+        else { document.getElementById('i2').setAttribute("style", "transform:rotate(0deg);"); }
+    }
+    else if (e == 3) {
+        if (i == 0) { document.getElementById('i3').setAttribute("style", "transform:rotate(90deg);"); }
+        else { document.getElementById('i3').setAttribute("style", "transform:rotate(0deg);"); }
+    }
+    else {
+        if (i == 0) { document.getElementById('i4').setAttribute("style", "transform:rotate(90deg);"); }
+        else { document.getElementById('i4').setAttribute("style", "transform:rotate(0deg);"); }
     }
 }
 
@@ -149,6 +177,23 @@ submit.onclick = (e) => {// main function
     }
 
 
+    // removeflexbasis();
+    // let flexbasis1 = document.getElementById("flexbasis1");
+    // let fb1 = flexbasis1.value;
+    // flexbasis1.classList.add("basis-" + fb1);
+
+    // let flexbasis2 = document.getElementById("flexbasis2");
+    // let fb2 = flexbasis2.value;
+    // flexbasis2.classList.add("basis-" + fb2);
+
+    // let flexbasis3 = document.getElementById("flexbasis3");
+    // let fb3 = flexbasis1.value;
+    // flexbasis3.classList.add("basis-" + fb3);
+
+    // let flexbasis4 = document.getElementById("flexbasis4");
+    // let fb4 = flexbasis4.value;
+    // flexbasis1.classList.add("basis-" + fb4);
+
     //because of error i value is not working in boxarray[]
 
         // let temp = boxarray[3][0].value;
@@ -184,9 +229,26 @@ submit.onclick = (e) => {// main function
         //     console.log(boxclassarray[3][temp]);
         // }// 3 = box no & i = property no 
     
-
 }
-
+//flexbasis
+// function removeflexbasis() {
+//     let flexbasis1 = document.getElementById("flexbasis1");
+//     let flexbasis2 = document.getElementById("flexbasis2");
+//     let flexbasis3 = document.getElementById("flexbasis3");
+//     let flexbasis4 = document.getElementById("flexbasis4");
+//     for (let i = 0; i < 13; i++){
+//         flexbasis1.classList.remove("basis-" + i);
+//     }
+//     for (let i = 0; i < 13; i++){
+//         flexbasis2.classList.remove("basis-" + i);
+//     }
+//     for (let i = 0; i < 13; i++){
+//         flexbasis3.classList.remove("basis-" + i);
+//     }
+//     for (let i = 0; i < 13; i++){
+//         flexbasis4.classList.remove("basis-" + i);
+//     }
+// }
 
 //combine function to remove class
 function removeclass(e) {
@@ -230,4 +292,54 @@ function removeclassforbox(a, b) {
            box[a].classList.remove(boxclassarray[b][i]); 
         }
     }
+}
+
+let a = 0, b = 0, c = 0;
+let x = 0, y = 0, z = 0;
+let p = 1, q = 1, r = 1;
+setInterval(function () {
+    randomy();
+    b1.style.backgroundColor = "rgb(" + a + "," + b + "," + c + ")";
+    b1.style.color = "rgb(" + x + "," + y + "," + z + ")";
+    randomy();
+    b2.style.backgroundColor = "rgb(" + a + "," + b + "," + c + ")";
+    b2.style.color = "rgb(" + x + "," + y + "," + z + ")";
+    randomy();
+    b3.style.backgroundColor = "rgb(" + a + "," + b + "," + c + ")";
+    b3.style.color = "rgb(" + x + "," + y + "," + z + ")";
+    randomy();
+    b4.style.backgroundColor = "rgb(" + a + "," + b + "," + c + ")";
+    b4.style.color = "rgb(" + x + "," + y + "," + z + ")";
+}, 100);
+
+function randomy() {
+    if (p == 1) {
+        a = a + 5;
+        if (a > 200) { p = 0; }
+    } else {
+        a = a - 4;
+        if (a < 20) { p = 1; }
+    }
+    if (q == 1) {
+        b = b + 4;
+        if (b > 200) { q = 0; }
+    } else {
+        b = b - 3;
+        if (b < 20) { q = 1; }
+    }
+    if (r == 1) {
+        c = c + 3;
+        if (c > 200) { r = 0; }
+    } else {
+        c = c - 2;
+        if (c < 20) { r = 1; }
+    }
+    x = 255 - a;
+    x = Math.abs(x);
+    
+    y = 255 - b;
+    y = Math.abs(y);
+    
+    z = 255 - c;
+    z = Math.abs(z);
 }
